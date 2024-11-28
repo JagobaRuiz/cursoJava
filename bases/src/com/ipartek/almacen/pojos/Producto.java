8b8ab99d148a7a7c0f4d8aeedbb22eeca11c7d16
JagobaRuiz <jagoba.ruizab@elorrieta-errekamari.com>
2024-11-28 12:34:00 +0100

    DaoProductoSqlite ordenado, resumido, sin código repetido

--- bases/src/com/ipartek/almacen/pojos/Producto.java
+++ bases/src/com/ipartek/almacen/pojos/Producto.java
@@ -11,41 +11,70 @@ public class Producto implements Serializable {
 	private String nombre;
 	private BigDecimal precio;
 	private LocalDate fechaCaducidad;
-	public Producto(Long id, String nombre, BigDecimal precio, LocalDate fechaCaducidad) {
+	private Categoria categoria;
+	
+	public Producto(Long id, String nombre, BigDecimal precio, LocalDate fechaCaducidad, Categoria categoria) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 		this.precio = precio;
 		this.fechaCaducidad = fechaCaducidad;
+		this.categoria = categoria;
 	}
+	
+	public Producto(Long id, String nombre, BigDecimal precio, LocalDate fechaCaducidad) {
+		this(id, nombre, precio, fechaCaducidad, null);
+	}
+	
+	
 	public Long getId() {
 		return id;
 	}
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public BigDecimal getPrecio() {
 		return precio;
 	}
+	
 	public void setPrecio(BigDecimal precio) {
 		this.precio = precio;
 	}
+	
 	public LocalDate getFechaCaducidad() {
 		return fechaCaducidad;
 	}
+	
 	public void setFechaCaducidad(LocalDate fechaCaducidad) {
 		this.fechaCaducidad = fechaCaducidad;
 	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	
+	
+	
 	@Override
 	public int hashCode() {
-		return Objects.hash(fechaCaducidad, id, nombre, precio);
+		return Objects.hash(categoria, fechaCaducidad, id, nombre, precio);
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -55,9 +84,11 @@ public class Producto implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Producto other = (Producto) obj;
-		return Objects.equals(fechaCaducidad, other.fechaCaducidad) && Objects.equals(id, other.id)
-				&& Objects.equals(nombre, other.nombre) && Objects.equals(precio, other.precio);
+		return Objects.equals(categoria, other.categoria) && Objects.equals(fechaCaducidad, other.fechaCaducidad)
+				&& Objects.equals(id, other.id) && Objects.equals(nombre, other.nombre)
+				&& Objects.equals(precio, other.precio);
 	}
+
 	@Override
 	public String toString() {
 		return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", fechaCaducidad=" + fechaCaducidad
