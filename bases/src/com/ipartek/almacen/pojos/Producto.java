9ac68ba1c3961c884db0990dfd8b29fba9c2cdb2
JagobaRuiz <jagoba.ruizab@elorrieta-errekamari.com>
2024-11-28 13:13:34 +0100

    Añadida la tabla categoria, con su relación con la tabla producto

--- bases/src/com/ipartek/almacen/pojos/Producto.java
+++ bases/src/com/ipartek/almacen/pojos/Producto.java
@@ -6,13 +6,16 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 public class Producto implements Serializable {
+	
 	private static final long serialVersionUID = -1343735207294068533L;
+
 	private Long id;
 	private String nombre;
 	private BigDecimal precio;
 	private LocalDate fechaCaducidad;
-	private Categoria categoria;
 	
+	private Categoria categoria;
+
 	public Producto(Long id, String nombre, BigDecimal precio, LocalDate fechaCaducidad, Categoria categoria) {
 		super();
 		this.id = id;
@@ -25,40 +28,39 @@ public class Producto implements Serializable {
 	public Producto(Long id, String nombre, BigDecimal precio, LocalDate fechaCaducidad) {
 		this(id, nombre, precio, fechaCaducidad, null);
 	}
-	
-	
+
 	public Long getId() {
 		return id;
 	}
-	
+
 	public void setId(Long id) {
 		this.id = id;
 	}
-	
+
 	public String getNombre() {
 		return nombre;
 	}
-	
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-	
+
 	public BigDecimal getPrecio() {
 		return precio;
 	}
-	
+
 	public void setPrecio(BigDecimal precio) {
 		this.precio = precio;
 	}
-	
+
 	public LocalDate getFechaCaducidad() {
 		return fechaCaducidad;
 	}
-	
+
 	public void setFechaCaducidad(LocalDate fechaCaducidad) {
 		this.fechaCaducidad = fechaCaducidad;
 	}
-	
+
 	public Categoria getCategoria() {
 		return categoria;
 	}
@@ -67,9 +69,6 @@ public class Producto implements Serializable {
 		this.categoria = categoria;
 	}
 
-	
-	
-	
 	@Override
 	public int hashCode() {
 		return Objects.hash(categoria, fechaCaducidad, id, nombre, precio);
@@ -91,10 +90,8 @@ public class Producto implements Serializable {
 
 	@Override
 	public String toString() {
-		return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", fechaCaducidad=" + fechaCaducidad
-				+ "]";
+		return String.format("Producto [id=%s, nombre=%s, precio=%s, fechaCaducidad=%s, categoria=%s]", id, nombre,
+				precio, fechaCaducidad, categoria);
 	}
-	
-	
 
 }
