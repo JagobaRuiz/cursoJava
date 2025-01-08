38684481ea65a111eec1eb208311cf8a8fca82b1
JagobaRuiz <jagoba.ruizab@elorrieta-errekamari.com>
2025-01-08 08:50:40 +0100

    Crear proyecto Ipartex

---
+++ ipartekx/src/main/java/com/ipartex/IpartekxApplication.java
@@ -0,0 +1,13 @@
+package com.ipartex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpartekxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IpartekxApplication.class, args);
+	}
+
+}
---
+++ ipartekx/src/test/java/com/ipartex/IpartekxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipartex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpartekxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
